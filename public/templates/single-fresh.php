e52e95e641c0b657f2cd77ce4ec0028217e88a99
Mark Parnell <mark@sparkweb.com.au>
2020-05-05 16:24:45 +1000

    Updated single FRESH template to fall back to featured image if no video exists

--- public/templates/single-fresh.php
+++ public/templates/single-fresh.php
@@ -3,10 +3,10 @@
  * Custom template for displaying a single FRESH post.
  * Override this by copying it to your theme and making the desired changes.
  *
- * @link       https://sparkweb.com.au
- * @since      1.0.0
+ * @link	   https://sparkweb.com.au
+ * @since	  1.0.0
  *
- * @package    Spark_Cw_Fresh
+ * @package	Spark_Cw_Fresh
  * @subpackage Spark_Cw_Fresh/public/templates
  */
 
@@ -16,16 +16,25 @@ global $post;
 $meta = Spark_Cw_Fresh_Meta::get_post_meta();
 ?>
 <article <?php post_class('fresh-wrapper'); ?>>
-    <h1><?php the_title(); ?></h1>
-    <p class="scripture"><span class="reference"><?php echo $meta['scripture_reference']; ?></span> <?php echo $meta['scripture_quote']; ?></p>
-    <div class="video">
-        <iframe src="<?php echo $meta['video']; ?>" allowfullscreen="" width="100%" height="auto" frameborder="0"></iframe>
-    </div>
-    <div class="audio">
-        <audio src="<?php echo $meta['audio']; ?>" controls="controls"></audio>
-    </div>
-    <?php the_content(); ?>
+	<h1><?php the_title(); ?></h1>
+	<p class="scripture"><span class="reference"><?php echo $meta['scripture_reference']; ?></span> <?php echo $meta['scripture_quote']; ?></p>
+<?php
+if (!empty($meta['video'])) {
+?>
+	<div class="video">
+		<iframe src="<?php echo $meta['video']; ?>" allowfullscreen="" width="100%" height="auto" frameborder="0"></iframe>
+	</div>
+<?php
+} elseif (has_post_thumbnail($post)) {
+?>
+	<img src="<?php echo wp_get_attachment_image_url(get_post_thumbnail_id(), 'large'); ?>" alt="">
+<?php
+}
+?>
+	<div class="audio">
+		<audio src="<?php echo $meta['audio']; ?>" controls="controls"></audio>
+	</div>
+	<?php the_content(); ?>
 </article>
 <?php
-
 get_footer();
